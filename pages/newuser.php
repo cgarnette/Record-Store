68a7379d8075ac49a6dd8178ce20bbb6ccc06720
cgarnette <cgarnette18@gmail.com>
2018-03-08 01:34:43 -0500

    update newuser
    added back the CustomerClass

--- pages/newuser.php
+++ pages/newuser.php
@@ -166,6 +166,67 @@ echo "<h2>error connecting</h2>";
 	return $mysqli;
 }
 
+class CustomerClass
+{
+	// property declaration
+	private $firstname="";
+	private $lastname="";
+	private $email="";
+	private $password="";
+	private $ID=0;
+	// Constructor
+	public function __construct($ID,$firstname,$lastname,$email,$password)
+	{
+	$this->ID = $ID;
+	$this->firstname = $firstname;
+	$this->lastname = $lastname;
+	$this->email = $email;
+	$this->password = $password;
+	}
+	// Get methods
+	public function getID ()
+	{
+		return $this->ID;
+	}
+	public function getFirstname ()
+	{
+		return $this->firstname;
+	}
+	public function getLastname ()
+	{
+		return $this->lastname;
+	}
+	public function getEmail ()
+	{
+		return $this->email;
+	}
+	public function getPassword ()
+	{
+		return $this->password;
+	}
+	// Set methods
+	public function setID ($value)
+	{
+		$this->ID = $value;
+	}
+	public function setFirstname ($value)
+	{
+		$this->firstname = $value;
+	}
+	public function setLastname ($value)
+	{
+		$this->lastname = $value;
+	}
+	public function setEmail ($value)
+	{
+		$this->email = $value;
+	}
+	public function setPassword ($value)
+	{
+		$this->password = $value;
+	}
+} // End Customerclass
+
 ?>
 
 
