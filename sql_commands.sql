cda68b2293dea8532a3788dc51a20e546a8c4f5f
cgarnette <cgarnette18@gmail.com>
2018-05-30 23:39:19 -0400

    change made

--- sql_commands.sql
+++ sql_commands.sql
@@ -21,13 +21,13 @@ insert into Customers
 values(0, 'Admin', 'Admin', 'admin@jukebox.com', 'beyonce1232');
 
 insert into Products
-values(0, 'Year of the Gentleman', 'Ne-yos third studio album. Released in 2008', 6.95, 'images/YOTG.jpg');
+values(0, 'Year of the Gentlem', 'Year of The Gentleman. Ne-yos third studio album. Released in 2008', 6.95, 'images/YOTG.jpg');
 
 insert into Products
-values(1, 'BDay', 'Beyonces second studio album. Released in 2006', 5.75, 'images/bday.jpg');
+values(1, 'BDay', 'BDay. Beyonces second studio album. Released in 2006', 5.75, 'images/bday.jpg');
 
 insert into Products
-values(2, 'In a Perfect World', 'Keri Hilsons Debut album. Released in 2009', 2.75, 'images/h.jpg');
+values(2, 'In a Perfect World', 'In a Perfect World. Keri Hilsons Debut album. Released in 2009', 2.75, 'images/h.jpg');
 
 insert into Products
-values(3, 'My Beautiful Dark Twisted Fantasy', 'Kanye Wests fifth studio album. Released in 2010', 4.99, 'images/k1.jpg');
+values(3, 'My Beautiful Dark Tw', 'My Beautiful Dark Twisted Fantasy. Kanye Wests fifth studio album. Released in 2010', 4.99, 'images/k1.jpg');
